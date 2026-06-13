2444d8f2eccbc0ba8746437fc2211d33856c766f
Oliver Gerler <rockus@rockus.at>
2026-06-13 21:30:17 +0100

    cputemp: fix typo

--- cputemp/cputemp_mqtt.sh
+++ cputemp/cputemp_mqtt.sh
@@ -13,7 +13,7 @@ while true; do
   CPU_TEMP0_REM=$(($CPU_TEMP0 % 1000))
   CPU_TEMP0=$CPU_TEMP0_INT.$CPU_TEMP0_REM
 
-  mosquitto_pub -h $HOST -u $USER -P $PWD -r -t $TOPIC/$NODE/cpu_temp/config -m '{"name": '$NODE'_cpu_temp", "device_class": "temperature", "state_topic": "'$TOPIC'/'$NODE'/cpu_temp/state"}'
+  mosquitto_pub -h $HOST -u $USER -P $PWD -r -t $TOPIC/$NODE/cpu_temp/config -m '{"name": "'$NODE'_cpu_temp", "device_class": "temperature", "state_topic": "'$TOPIC'/'$NODE'/cpu_temp/state"}'
   mosquitto_pub -h $HOST -u $USER -P $PWD -t $TOPIC/$NODE/cpu_temp/state -m $CPU_TEMP0
 
   sleep 60
