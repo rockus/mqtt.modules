5b61c48828782c3e7a576efa0e8deea9dd385b4e
Oliver Gerler <rockus@rockus.at>
2026-06-13 22:21:51 +0200

    cputemp: add config msg for mqtt autodisovery

--- cputemp/cputemp_mqtt.sh
+++ cputemp/cputemp_mqtt.sh
@@ -2,6 +2,7 @@
 
 HOST="broker"
 NODE="node"
+TOPIC="homeassistant/sensor"
 USER="mqtt_user"
 PWD="mqtt_pwd"
 
@@ -12,7 +13,8 @@ while true; do
   CPU_TEMP0_REM=$(($CPU_TEMP0 % 1000))
   CPU_TEMP0=$CPU_TEMP0_INT.$CPU_TEMP0_REM
 
-  mosquitto_pub -h $HOST -u $USER -P $PWD -t $NODE/cpu_temp -m $CPU_TEMP0
+  mosquitto_pub -h $HOST -u $USER -P $PWD -r -t $TOPIC/$NODE/cpu_temp/config -m '{"name": '$NODE'_cpu_temp", "device_class": "temperature", "state_topic": "'$TOPIC'/'$NODE'/cpu_temp/state"}'
+  mosquitto_pub -h $HOST -u $USER -P $PWD -t $TOPIC/$NODE/cpu_temp/state -m $CPU_TEMP0
 
   sleep 60
 done
